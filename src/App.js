cdec205e3896cc0829cdca6dbfc5434a0d23d543
George Batalinski <george.batalinski@gmail.com>
2023-05-01 09:33:50 -0400

    clean up: remove not needed code

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ import DeckGL from '@deck.gl/react';
 import {PolygonLayer} from '@deck.gl/layers';
 import {TripsLayer} from '@deck.gl/geo-layers';
 
-import { invoke } from '@tauri-apps/api';
 const timer = require('./timepicker.js');
 let timepicker;
-let startTime = Date.now();
-let lastRAFTimestamp = 0;
 let intervalId;
 // Source data CSV
 const DATA_URL = {
@@ -79,14 +76,6 @@ export default function App({
 }) {
 
   const domElementRef = useRef(null);
-  // now we can call our Command!
-  // Right-click the application background and open the developer tools.
-  // You will see "Hello, World!" printed in the console!
-  invoke('greet', { name: 'World' })
-  // `invoke` returns a Promise
-  .then((response) => console.log(response))
-
-
   const [time, setTime] = useState(0);
   const [animation] = useState({});
   const lastRAFTimestamp = useRef(0);
